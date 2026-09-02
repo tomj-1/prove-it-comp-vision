78a56c18d8d187d106394bd8df565936b6656454
Piyou <tj67542007@gmail.com>
2026-09-01 21:46:40 -0700

    Update computer vision project

--- Main.py
+++ Main.py
@@ -1,20 +1,7 @@
 import cv2
 import argparse 
 class Main:
+    pass
     
-    def Image_Input(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input", required = True)
-        
-        args = parser.parse_args()
-        image = cv2.imread(args.input)
-        
-        if image is None:
-            print("couldn't process image")
-            exit()
-            
-        return image 
-    
-main = Main()
-print(main.Image_Input())
+
         
--- Yolov2Model.py
+++ Yolov2Model.py
@@ -1,6 +1,45 @@
+import torch
+import argparse
+import Main
+from utils.utils import  select_device
+class ImageProcessing():
+        
+    
+    def __init__(self):
+        
+          
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--weights', nargs='+', type=str, default='data/weights/yolopv2.pt', help='model.pt path(s)')
+        parser.add_argument('--source', type=str, default='inference/vid2', help='source')  # file/folder, 0 for webcam
+        parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+        parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
+        parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+        parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+        parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+        parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+        parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+        parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+        parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+        parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+        parser.add_argument('--name', default='exp', help='save results to project/name')
+        parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+        self.opt = parser.parse_args()
+        # setting and directories
+        self.source, self.weights,  save_txt, imgsz = self.opt.source, self.opt.weights,  self.opt.save_txt, self.opt.img_size
+        save_img = not self.opt.nosave and not self.source.endswith('.txt')  # save inference images
+        
+            # Load model
+        stride =32
+        self.model  = torch.jit.load(self.weights)
+        self.device = select_device(self.opt.device)
+        half = self.device.type != 'cpu'  # half precision only supported on CUDA
+        self.model = self.model.to(self.device)
+               
 
-class ImageProcessing:
-    def _init_(self):
+    def load_image(self):
+        pass
+
+    def preprocess_image(self):
         pass
     
 
